72d40f75a266be11da2789c0c22850b67bc86fe4
Cristóbal Contreras Rubio <crisconru@gmail.com>
2024-01-31 11:10:03 +0100

    numbers schemas tested

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,21 +1,72 @@
 import * as v from 'valibot'
-import { INT16_MAX, INT16_MIN, INT32_MAX, INT32_MIN, INT8_MAX, INT8_MIN } from './constants'
+import { INT16_MAX, INT16_MIN, INT32_MAX, INT32_MIN, INT8_MAX, INT8_MIN, UINT16_MAX, UINT32_MAX, UINT8_MAX } from './constants'
 // COMMONS
 export const StringSchema = v.string()
 export const StringArraySchema = v.array(StringSchema)
 export const BooleanSchema = v.boolean()
 export const NumberSchema = v.number()
 // INTEGERS
-export const IntegerSchema = v.number([v.integer()])
-export const Int8Schema = v.number([v.integer(), v.minValue(INT8_MIN), v.maxValue(INT8_MAX)])
-export const Iint16Schema = v.number([v.integer(), v.minValue(INT16_MIN), v.maxValue(INT16_MAX)])
-export const Int32Schema = v.number([v.integer(), v.minValue(INT32_MIN), v.maxValue(INT32_MAX)])
-// export const Int64Schema = IntegerSchema.min(INT64_MIN).max(INT64_MAX)
-export const BigIntegerSchema = v.bigint()
-// NATURALS
-export const NaturalSchema = v.number([v.integer(), v.minValue(0)])
-export const Uint8Schema = v.number([v.integer(), v.minValue(0), v.maxValue(INT8_MAX)])
-export const Uint16Schema = v.number([v.integer(), v.minValue(0), v.maxValue(INT16_MAX)])
-export const Uint32Schema = v.number([v.integer(), v.minValue(0), v.maxValue(INT32_MAX)])
-// export const Uint64Schema = NaturalSchema.max(UINT64_MAX)
-export const BigUintSchema = v.bigint([v.minValue(0n)])
+export const Int8Schema = v.number(
+  'It should be a number',
+  [
+    v.integer('It should be an integer number'),
+    v.minValue(INT8_MIN, `It should be greater than ${INT8_MIN - 1}`),
+    v.maxValue(INT8_MAX, `It should be less than ${INT8_MAX + 1}`)
+  ]
+)
+export const Int16Schema = v.number(
+  'It should be a number',
+  [
+    v.integer('It should be an integer number'),
+    v.minValue(INT16_MIN, `It should be greater than ${INT16_MIN - 1}`),
+    v.maxValue(INT16_MAX, `It should be less than ${INT16_MAX + 1}`)
+  ]
+)
+export const Int32Schema = v.number(
+  'It should be a number',
+  [
+    v.integer('It should be an integer number'),
+    v.minValue(INT32_MIN, `It should be greater than ${INT32_MIN - 1}`),
+    v.maxValue(INT32_MAX, `It should be less than ${INT32_MAX + 1}`)
+  ]
+)
+export const BigIntSchema = v.bigint('It should be a BigInt')
+// UNSIGNED INTEGERS
+export const Uint8Schema = v.number(
+  'It should be a number',
+  [
+    v.integer('It should be an integer number'),
+    v.minValue(0, 'It should be greater than 0'),
+    v.maxValue(UINT8_MAX, `It should be less than ${UINT8_MAX + 1}`)
+  ]
+)
+export const Uint16Schema = v.number(
+  'It should be a number',
+  [
+    v.integer('It should be an integer number'),
+    v.minValue(0, 'It should be greater than 0'),
+    v.maxValue(UINT16_MAX, `It should be less than ${UINT16_MAX + 1}`)
+  ]
+)
+export const Uint32Schema = v.number(
+  'It should be a number',
+  [
+    v.integer('It should be an integer number'),
+    v.minValue(0, 'It should be greater than 0'),
+    v.maxValue(UINT32_MAX, `It should be less than ${UINT32_MAX + 1}`)
+  ]
+)
+export const BigUintSchema = v.bigint(
+  'It should be a BigInt',
+  [v.minValue(0n, 'It should be greater than 0n')]
+)
+// HARDWARE
+export const SerialNumberSchema = v.string(
+  'It should be a string',
+  [
+    v.custom((input: string) => {
+      const num = Number(input)
+      return !Number.isNaN(num) && Number.isInteger(num) && num > -1
+    }, 'It should be a positive integer string number')
+  ]
+)
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import * as v from 'valibot'
-import { BigUintSchema, Iint16Schema, Int32Schema, Int8Schema, Uint16Schema, Uint32Schema, Uint8Schema } from "./schemas";
+import { BigUintSchema, Iint16Schema, Int32Schema, Int8Schema, SerialNumberSchema, Uint16Schema, Uint32Schema, Uint8Schema } from "./schemas";
 // INTEGERS
 export type Int8 = v.Input<typeof Int8Schema>
 export type Int16 = v.Input<typeof Iint16Schema>
@@ -9,3 +9,18 @@ export type Uint8 = v.Input<typeof Uint8Schema>
 export type Uint16 = v.Input<typeof Uint16Schema>
 export type Uint32 = v.Input<typeof Uint32Schema>
 export type BigUint = v.Input<typeof BigUintSchema>
+// Hydrophone
+export type SerialNumber = v.Input<typeof SerialNumberSchema>
+
+
+// export type Sensor = {
+//   serialNumber: 
+// }
+
+
+
+
+
+// export interface  {
+//   parseData: (input: string): 
+// }
